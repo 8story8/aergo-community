fe0d84e9884e32a4593df34bffa22ad957fceabb
8story8 <8story8@blocko.io>
2020-07-13 20:39:27 +0900

    add aergo setting 1

--- aergo-community-api/src/main/java/io/blocko/AergoCommunityServerLauncher.java
+++ aergo-community-api/src/main/java/io/blocko/AergoCommunityServerLauncher.java
@@ -1,4 +1,4 @@
-package io.saiden;
+package io.blocko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- aergo-community-api/src/main/java/io/blocko/config/WebSecurityConfig.java
+++ aergo-community-api/src/main/java/io/blocko/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package io.saiden.config;
+package io.blocko.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
