fb476d3fa6bde11e29c0fc712284f351f54a2ec3
8story8 <8story8@blocko.io>
2020-07-28 11:15:08 +0900

    modify remove aergo data to remove aergo

--- aergo/remove.sh
+++ aergo/remove.sh
@@ -4,7 +4,7 @@ SCRIPT_HOME="$( cd "$(dirname "$0")" ; pwd -P )"
 
 AERGO_IMAGE_ID=$(docker images -q aergo/node:2.2.3)
 
-function remove_aergo_data {
+function remove_aergo {
   AERGO_DATA=$SCRIPT_HOME/blockchain/data
   if [ -d $AERGO_DATA ]; then
     rm -rf $AERGO_DATA
@@ -30,11 +30,11 @@ if [ ! -z $AERGO_IMAGE_ID ]; then
   # Aergo가 구동 중이지 않은 경우
   if [ -z $AERGO_CONTAINER_ID ]; then
     docker rmi $AERGO_IMAGE_ID
-    remove_aergo_data
+    remove_aergo
   # Aergo가 구동 중인 경우
   else
     docker rm -f $AERGO_CONTAINER_ID
     docker rmi $AERGO_IMAGE_ID
-    remove_aergo_data
+    remove_aergo
   fi
 fi
--- aergo/run.sh
+++ aergo/run.sh
@@ -4,7 +4,7 @@ SCRIPT_HOME="$( cd "$(dirname "$0")" ; pwd -P )"
   
 AERGO_IMAGE_ID=$(docker images -q aergo/node:2.2.3)
 
-function remove_aergo_data {
+function remove_aergo {
   AERGO_DATA=$SCRIPT_HOME/blockchain/data
   if [ -d $AERGO_DATA ]; then
     rm -rf $AERGO_DATA
@@ -31,7 +31,7 @@ function init_aergo {
 
 # Aergo Image가 없을 경우
 if [ -z $AERGO_IMAGE_ID ]; then
-  remove_aergo_data
+  remove_aergo
   init_aergo
 
 # Aergo Image가 있을 경우
@@ -40,14 +40,14 @@ else
    
   # Aergo가 구동 중이지 않은 경우
   if [ -z $AERGO_CONTAINER_ID ]; then
-    remove_aergo_data
+    remove_aergo
     init_aergo
   # Aergo가 구동 중인 경우
   else
     AERGO_CONTAINER_EXIT_CODE=$(docker inspect $AERGO_CONTAINER_ID --format='{{.State.ExitCode}}')
     # Aergo Container에 오류가 발생했을 경우
     if [ $AERGO_CONTAINER_EXIT_CODE -ne 0 ]; then
-      remove_aergo_data
+      remove_aergo
       init_aergo
     # Aergo Container에 오류가 발생하지 않았을 경우
     else
