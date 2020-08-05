4b97275b0ee93db71b4101580ed3c47f3308efe0
8story8 <8story8@blocko.io>
2020-08-05 23:56:23 +0900

    add sort board list

--- aergo-community-api/src/main/java/io/blocko/service/BoardService.java
+++ aergo-community-api/src/main/java/io/blocko/service/BoardService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -49,7 +50,7 @@ public class BoardService {
 
 	public Page<Board> findAll(Pageable pageable) {
 		int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
-		pageable = PageRequest.of(page, 10);
+		pageable = PageRequest.of(page, 10, Sort.by("id").descending());
 		return boardRepository.findAll(pageable);
 	}
 	
